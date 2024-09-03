d1068566bc1e6465a8d9afc9edc21ad473325f74
rojanagunoori <nagunooriroja@gmail.com>
2024-09-03 10:48:12 +0530

    time checking 2nd time

--- config/db.js
+++ config/db.js
@@ -1,80 +1,5 @@
 const mongoose = require('mongoose');
 const User = require('../models/userModel');
-const bcrypt = require("bcryptjs");
-const Grid = require('gridfs-stream');
-
-let gfs;
-
-const connectDB = async () => {
-    const url = process.env.MONGO_URI || "mongodb+srv://guest-posting-marketplace-web:guest-posting-marketplace-web@cluster0.kjvasef.mongodb.net/guest-posting-marketplace-web?retryWrites=true&w=majority&appName=Cluster0";
-    
-    try {
-        // Start timing the database connection
-        const dbConnectStart = Date.now();
-        const conn = await mongoose.connect(url, {
-            dbName: "guest-posting-marketplace-web"
-        });
-        const dbConnectEnd = Date.now();
-        console.log(`Database connection took ${dbConnectEnd - dbConnectStart} ms`);
-
-        gfs = Grid(conn.connection.db, mongoose.mongo);
-        gfs.collection('uploads');
-        console.log('MongoDB connected');
-
-        // Start timing Super Admin check
-        const superAdminCheckStart = Date.now();
-        const superAdmin = await User.findOne({ role: 'Super Admin' });
-        const superAdminCheckEnd = Date.now();
-        console.log(`Checking for Super Admin took ${superAdminCheckEnd - superAdminCheckStart} ms`);
-        
-        if (!superAdmin) {
-            const createSuperAdminStart = Date.now();
-            const defaultSuperAdmin = new User({
-                name: 'Default Super Admin',
-                email: 'superadmin@example.com',
-                password: await bcrypt.hash('superadminpassword', 10),
-                role: 'Super Admin'
-            });
-            await defaultSuperAdmin.save();
-            const createSuperAdminEnd = Date.now();
-            console.log(`Creating Super Admin took ${createSuperAdminEnd - createSuperAdminStart} ms`);
-        } else {
-            console.log('Default Super Admin already created');
-        }
-
-        // Start timing Admin check
-        const adminCheckStart = Date.now();
-        const admin = await User.findOne({ role: 'Admin' });
-        const adminCheckEnd = Date.now();
-        console.log(`Checking for Admin took ${adminCheckEnd - adminCheckStart} ms`);
-        
-        if (!admin) {
-            const createAdminStart = Date.now();
-            const defaultAdmin = new User({
-                name: 'Default Admin',
-                email: 'admin@example.com',
-                password: await bcrypt.hash('adminpassword', 10),
-                role: 'Admin'
-            });
-            await defaultAdmin.save();
-            const createAdminEnd = Date.now();
-            console.log(`Creating Admin took ${createAdminEnd - createAdminStart} ms`);
-        } else {
-            console.log('Default Admin already created');
-        }
-
-    } catch (err) {
-        console.error(err.message);
-        process.exit(1);
-    }
-};
-
-module.exports = connectDB;
-
-
-
-/*const mongoose = require('mongoose');
-const User = require('../models/userModel');
 const bcrypt=require("bcryptjs")
 const Grid = require('gridfs-stream');
 
@@ -83,24 +8,33 @@ let gfs;
 const connectDB = async () => {
     const url=process.env.MONGO_URI || "mongodb+srv://guest-posting-marketplace-web:guest-posting-marketplace-web@cluster0.kjvasef.mongodb.net/guest-posting-marketplace-web?retryWrites=true&w=majority&appName=Cluster0"
     try {
-        const dbConnectStart = Date.now();
+        const dbConnectStart = process.hrtime();
         const conn =  await mongoose.connect(url, {
         
             dbName:"guest-posting-marketplace-web",
-           // useNewUrlParser: true,
-           // useUnifiedTopology: true, 
-           // poolSize: 10, 
-          //  serverSelectionTimeoutMS: 5000,
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+            poolSize: 10, 
+            serverSelectionTimeoutMS: 5000
         });
+        const dbConnectEnd = process.hrtime(dbConnectStart);
+        console.log(`Database connection took ${dbConnectEnd[0]} seconds and ${dbConnectEnd[1] / 1000000} milliseconds`);
+
         
         
-        const dbConnectEnd = Date.now();
-        console.log(`Database connection took ${dbConnectEnd - dbConnectStart} ms`);
+
         gfs = Grid(conn.connection.db, mongoose.mongo);
         gfs.collection('uploads');
         console.log('MongoDB connected');
 
+        const superAdminCheckStart = process.hrtime();
+
         const superAdmin = await User.findOne({ role: 'Super Admin' });
+
+        const superAdminCheckEnd = process.hrtime(superAdminCheckStart);
+        console.log(`Checking for Super Admin took ${superAdminCheckEnd[0]} seconds and ${superAdminCheckEnd[1] / 1000000} milliseconds`);
+        
+
         if (!superAdmin) {
             const defaultSuperAdmin = new User({
                 name: 'Default Super Admin',
@@ -113,7 +47,12 @@ const connectDB = async () => {
         }else{
             console.log('Default Super Admin already created');
         }
+
+        const adminCheckStart = process.hrtime();
         const admin = await User.findOne({ role: 'Admin' });
+        const adminCheckEnd = process.hrtime(adminCheckStart);
+        console.log(`Checking for Admin took ${adminCheckEnd[0]} seconds and ${adminCheckEnd[1] / 1000000} milliseconds`);
+        
         if (!admin) {
             const defaultSuperAdmin = new User({
                 name: 'Default Admin',
@@ -150,7 +89,7 @@ const connectDB = async () => {
             mongoose.disconnect();
           }
           
-          dropIndex();///end place
+          dropIndex();*/
     } 
      catch (err) {
         console.error(err.message);
@@ -158,5 +97,5 @@ const connectDB = async () => {
     }
 };
 
-module.exports = connectDB;*/
+module.exports = connectDB;
 
