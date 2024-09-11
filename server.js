985613ea553d17756face79717418fb57736bb89
rojanagunoori <nagunooriroja@gmail.com>
2024-09-11 22:09:45 +0530

    url not working

--- server.js
+++ server.js
@@ -44,8 +44,8 @@ app.use(cors({
 /*app.use(cors({
   origin: 'http://localhost:3000',
   credentials: true, 
-}));*/
-
+}));
+*/
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
